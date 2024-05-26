4b10e1b87e0d251d06bc18407f22cb391d72da28
Виктор <vapanfilov@gmail.com>
2024-05-26 17:23:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#Задача «Длина слова».
+a = '123456'
+print(len(a))
+
+#Задача «Суммы и разности».
+first = 5
+second = 3
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+#Задача «Среднее арифметическогое».
+first = 5
+second = 3
+third = 4
+mean = (first + second + third)/3
+print(mean)
+
+#Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ', ' + first_string)
+
+#Задача «Сложная формула».
+a = 1
+b = 2
+c = 3
+f = (a * b) + (a * c)
+print((f**3)/2)
+
