5711c4663db262adc141f4ec490933a98d6b80e9
Виктор <vapanfilov@gmail.com>
2024-06-02 15:24:53 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,8 @@
+number_of_ready_home_works = 12
+number_of_hours = 1.5
+name_of_course = 'Python'
+hours_per_one_task = number_of_hours / number_of_ready_home_works
+print('Курс:', name_of_course + ',',
+'всего задач:', str(number_of_ready_home_works) + ',',
+'затрачено часов:', str(number_of_hours)+',',
+'среднее время выполнения', hours_per_one_task, 'часа.')
