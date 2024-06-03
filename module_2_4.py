32ab27867775b143514eceb8acb948f163c11f8a
Виктор <vapanfilov@gmail.com>
2024-06-03 18:38:14 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -5,9 +5,9 @@ for x in numbers:
     if x < 2:
         continue
     is_prime = True
-    for i in range(2, x):
+    # for i in range(2, x):
     # с точки зрения математики тут можно было бы написать
-    # for i in range(2, int(x ** 0.5) + 1):
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             is_prime = False
             break
