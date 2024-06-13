ed3b343ef81432176a8967632d5e3faa782909e7
Виктор <vapanfilov@gmail.com>
2024-06-13 21:15:21 +0300

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,29 @@
+def calculate_structure_sum(*args, **kwargs):
+    sum = 0
+    for x in args:
+        if isinstance(x, int):
+            sum += x
+        elif isinstance(x, float):
+            sum += x
+        elif isinstance(x, str):
+            sum += len(x)
+        elif isinstance(x, (list, tuple, set)):
+            sum += calculate_structure_sum(*x)
+        elif isinstance(x, dict):
+            sum += calculate_structure_sum(**x)
+    for key, value in kwargs.items():
+        sum += calculate_structure_sum(key)
+        sum += calculate_structure_sum(value)
+    return sum
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
