5d03aa7fefc9e7ff8675bdc14e10b624e7d5236f
Виктор <vapanfilov@gmail.com>
2024-06-24 14:55:25 +0300

    Add files via upload

---
+++ interactivity/DrawReadKeyboard.dpr
@@ -0,0 +1,40 @@
+{$APPTYPE CONSOLE}
+program DrawReadKeyboard;
+ uses Winapi.Windows;
+
+ var
+  h: THandle;
+  x: dword;
+  e: TInputRecord;
+
+ // ѕолучить символ нажатой клавиши. ≈сли ничего не нажато, то возвращаетс€ #0.
+ // »спользуетс€ дл€ добавлени€ интерактивности в консольное приложение,
+ // например дл€ получени€ информации о текущем состо€нии работающей программы.
+ function ReadKeyboard: char;
+  begin
+   Result:=#0;
+   repeat
+    GetNumberOfConsoleInputEvents(h, x);
+    if x=0 then exit;
+    ReadConsoleInput(h, e, 1, x);
+    if (e.EventType = KEY_EVENT) and e.Event.KeyEvent.bKeyDown then
+     begin
+      Result:= e.Event.KeyEvent.UnicodeChar;
+      exit
+     end
+   until false
+  end;
+
+ var
+  c: char;
+
+BEGIN
+  h:= GetStdHandle(STD_INPUT_HANDLE);
+  FlushConsoleInputBuffer(h);
+
+  repeat
+   c:= ReadKeyboard;
+   if c <> #0 then writeln(ord(c))
+  until false
+
+END.
