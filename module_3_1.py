24e86b54849de422bd65647949e2e1fd32b910a3
Виктор <vapanfilov@gmail.com>
2024-08-03 13:46:19 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,30 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    return
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    x = string.lower()
+    for i in list_to_search:
+        if type(i) != str:
+            continue
+        if x == i.lower():
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
